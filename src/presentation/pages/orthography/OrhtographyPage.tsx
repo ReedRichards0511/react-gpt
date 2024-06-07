1c471595c110d207c2f1f3558844fce4f2f66fb1
Luis Mauricio Matango Mendoza <lmatango@ec.krugercorp.com>
2024-06-07 16:19:31 -0500

    add input form

--- src/presentation/pages/orthography/OrhtographyPage.tsx
+++ src/presentation/pages/orthography/OrhtographyPage.tsx
@@ -1,4 +1,5 @@
 import { GPTMessage, MyMessage, TypingLoader } from "../../components"
+import { TextMessageBox } from "../../components/chat-input-boxes/TextMessageBox"
 
 export const OrhtographyPage = () => {
   return (
@@ -9,11 +10,20 @@ export const OrhtographyPage = () => {
           {/* Bienvenida */}
 
           <GPTMessage text="¡Hola! Soy un modelo de lenguaje de OpenAI y estoy aquí para ayudarte con tus dudas de ortografía. ¿En qué puedo ayudarte hoy?" />
-          <MyMessage text ="hola mundo"/>
-        
-          <TypingLoader className="fade-in"/>
+          <MyMessage text="hola mundo" />
+
+          <TypingLoader className="fade-in" />
         </div>
       </div>
+
+      <TextMessageBox
+        onSendMessage={message => console.log(message)}
+        placeholder="Escribe aqui lo que desees"
+        disableCorrections
+        
+      />
+
+
     </div>
   )
 }
